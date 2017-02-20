51d42eec78fa52c9d915020d44d5c2cb5309a678
Roylisto Putra Pradana <roylisto@ymail.com>
2017-02-20 14:27:21 +0800

    Add SpringBootApplication.java

---
+++ university-web/src/main/java/com/roylisto/launcher/SpringBootApplication.java
@@ -0,0 +1,28 @@
+package com.roylisto.launcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({"com.roylisto"})
+@EnableJpaRepositories({"com.roylisto"})
+@EntityScan({"com.roylisto"})
+public class SpringBootApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		
+		return application.sources(SpringBootApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+
+}
